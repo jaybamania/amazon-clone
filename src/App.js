ab44c90911e253d8e69abbf2657ae4aad17b680e
jaybamania <jaykumardiu@gmail.com>
2021-05-13 16:49:57 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am the Checkout, Smash the Like Button</h1> 
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
